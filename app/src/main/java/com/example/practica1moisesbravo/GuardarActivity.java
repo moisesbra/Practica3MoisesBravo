f84b845d9fb928e1b0eea37dbd74255b5b8ff458
moisesbra <57927619+moisesbra@users.noreply.github.com>
2019-11-29 14:54:07 -0500

    Salu2 profe

---
+++ app/src/main/java/com/example/practica1moisesbravo/GuardarActivity.java
@@ -0,0 +1,14 @@
+package com.example.practica1moisesbravo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuardarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guardar);
+    }
+}
---
+++ app/src/main/java/com/example/practica1moisesbravo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.practica1moisesbravo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonLogin, buttonGuardar, buttonBuscar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
+        buttonGuardar = (Button)findViewById(R.id.buttonGuardar);
+        buttonBuscar = (Button)findViewById(R.id.buttonBuscar);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GuardarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BuscarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
