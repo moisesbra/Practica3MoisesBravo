fa70372e21285db21b759b47d55a57cd7af4d6bf
moisesbra <57927619+moisesbra@users.noreply.github.com>
2019-11-30 15:41:02 -0500

    Practica 3

--- app/src/main/java/com/example/practica1moisesbravo/MainActivity.java
+++ app/src/main/java/com/example/practica1moisesbravo/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.practica1moisesbravo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonLogin, buttonGuardar, buttonBuscar, buttonParametro;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonLogin = (Button)findViewById(R.id.buttonLogin);
-        buttonGuardar = (Button)findViewById(R.id.buttonGuardar);
-        buttonBuscar = (Button)findViewById(R.id.buttonBuscar);
-        buttonParametro = (Button)findViewById(R.id.buttonPasarParametro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonGuardar = findViewById(R.id.buttonGuardar);
+        buttonBuscar = findViewById(R.id.buttonBuscar);
+        buttonParametro = findViewById(R.id.buttonPasarParametro);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,5 +59,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()){
+            case R.id.opcionLogin:
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.opcionGuardar:
+            intent = new Intent(MainActivity.this, GuardarActivity.class);
+            startActivity(intent);
+            break;
+            case R.id.opcionBuscar:
+                intent = new Intent(MainActivity.this, BuscarActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.opcionPasarparatmetro:
+                intent = new Intent(MainActivity.this, PasarParametroActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+        return true;
     }
 }
